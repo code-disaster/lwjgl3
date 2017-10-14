8328859382f68000bee798c6bb16d2bd4cbc6c15
Daniel Ludwig <codi@code-disaster.com>
2017-10-14 23:48:32 +0200

    refine build script, building lwjgl, stb and xxhash as Jigsaw modules

---
+++ modules/core/src/jigsaw/lwjgl-stb/java/module-info.java
@@ -0,0 +1,4 @@
+module org.lwjgl.util.stb {
+	requires org.lwjgl;
+    exports org.lwjgl.stb;
+}
---
+++ modules/core/src/jigsaw/lwjgl-xxhash/java/module-info.java
@@ -0,0 +1,4 @@
+module org.lwjgl.util.xxhash {
+	requires org.lwjgl;
+    exports org.lwjgl.util.xxhash;
+}
